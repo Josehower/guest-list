0dab91b2ccb1399a19f7c20b204738183633caad
Josehower <josehowerupn@gmail.com>
2020-09-27 16:32:22 +0200

    Create all fucntionality and still need style

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import List from './List';
 function App() {
-  const [guestList, setGuestList] = useState([
-    ['mariana', 'leiva', true, Math.random()],
-    ['jose', 'ordoniez', false, Math.random()],
-    ['marcela', 'yepes', false, Math.random()],
-  ]);
+  const [guestList, setGuestList] = useState([]);
   const [newGuestFirstName, setNewGuestFirstName] = useState('');
   const [newGuestLastName, setNewGuestLastName] = useState('');
+  const [indexOnEditMode, setIndexOnEditMode] = useState('');
+  const [actualDateInput, setActualDateInput] = useState(false);
+  const [dateIdRef, setDateIdRef] = useState([]);
 
   function addGuest(e) {
     e.preventDefault();
+    setIndexOnEditMode('');
     const areGuestNamesEmpty =
       newGuestFirstName === '' || newGuestLastName === '' ? true : false;
 
     if (areGuestNamesEmpty) {
       return;
     }
+    async function postGuest() {
+      const response = await fetch(`https://jh-guest-list.herokuapp.com/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: newGuestFirstName,
+          lastName: newGuestLastName,
+        }),
+      });
+      const newGuest = await response.json();
+      const newGuestIdItem = [
+        { id: newGuest.id, date: actualDateInput },
+        ...dateIdRef,
+      ];
+      localStorage.setItem('dateIdRef', JSON.stringify(newGuestIdItem));
+      setDateIdRef(newGuestIdItem);
 
-    const stateBackup = guestList;
-    setGuestList([
-      [newGuestFirstName, newGuestLastName, false, Math.random() + new Date()],
-      ...stateBackup,
-    ]);
+      mirrorState();
+    }
+    postGuest();
   }
+
+  function mirrorState() {
+    async function fetchData() {
+      const response = await (
+        await fetch('https://jh-guest-list.herokuapp.com/')
+      ).json();
+      setGuestList(response);
+    }
+    fetchData();
+  }
+
+  useEffect(() => {
+    mirrorState();
+  }, []);
+
+  useEffect(() => {
+    setDateIdRef(
+      localStorage.getItem('dateIdRef')
+        ? JSON.parse(localStorage.getItem('dateIdRef'))
+        : [],
+    );
+  }, []);
 
   return (
     <>
@@ -42,8 +80,22 @@ function App() {
           type="text"
           placeholder="Last Name"
         />
+        <input
+          onChange={(e) => setActualDateInput(e.currentTarget.value)}
+          type="datetime-local"
+          name="deadLine"
+          value={actualDateInput}
+        />
       </form>
-      <List guestList={guestList} setGestList={setGuestList} />
+      <List
+        guestList={guestList}
+        mirrorState={mirrorState}
+        setGuestList={setGuestList}
+        indexOnEditMode={indexOnEditMode}
+        setIndexOnEditMode={setIndexOnEditMode}
+        setDateIdRef={setDateIdRef}
+        dateIdRef={dateIdRef}
+      />
     </>
   );
 }
--- src/List.js
+++ src/List.js
@@ -2,12 +2,19 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const Form = styled.form`
-  ${(props) => (props.isAssistent ? 'background: green;' : 'background: red;')}
+  width: 50vw;
+  div {
+    display: inline;
+    color: black;
+  }
   ${(props) =>
-    props.isAssistFilterOn && !props.isAssistent ? 'display: none;' : ''}
-  ${(
-    props,
-  ) => (props.isNonAssistFilterOn && props.isAssistent ? 'display: none;' : '')}
+    props.isAttendent
+      ? 'background: #90ee90; div {display: none;}'
+      : 'background: #FFA6A6;'}
+  ${(props) =>
+    props.isAttendFilterOn && !props.isAttendent ? 'display: none;' : ''}
+  ${(props) =>
+    props.isNonAttendFilterOn && props.isAttendent ? 'display: none;' : ''}
 `;
 
 const EditButton = styled.button`
@@ -15,119 +22,203 @@ const EditButton = styled.button`
 `;
 
 const List = (props) => {
-  const [isAssistFilterOn, setIsAssistFilterOn] = useState(false);
-  const [isNonAssistFilterOn, setIsNonAssistFilterOn] = useState(false);
-  const [indexOnEditMode, setIndexOnEditMode] = useState('');
+  const [isAttendFilterOn, setIsAttendFilterOn] = useState(false);
+  const [isNonAttendFilterOn, setIsNonAttendFilterOn] = useState(false);
 
-  function removeGuest(e, index) {
+  async function removeGuest(e, guest) {
     e.preventDefault();
-    let stateBackup = props.guestList;
-    stateBackup = stateBackup.filter(
-      (guest, guestIndex) => guestIndex !== index,
+    const response = await fetch(
+      `https://jh-guest-list.herokuapp.com/${guest.id}`,
+      { method: 'DELETE' },
     );
-    props.setGestList([...stateBackup]);
+    await response.json();
+    const dateIdRefStorage = JSON.parse(localStorage.getItem('dateIdRef'));
+    const newRef = dateIdRefStorage.filter(({ id }) => id !== guest.id);
+    props.setDateIdRef(newRef);
+    localStorage.setItem('dateIdRef', JSON.stringify(newRef));
+    props.mirrorState();
   }
 
   function editGuest(e, index) {
-    setIndexOnEditMode(index);
+    e.preventDefault();
+    props.mirrorState();
+    props.setIndexOnEditMode(index);
   }
 
-  function doneEditGuest(e) {
+  async function doneEditGuest(e, guest) {
     e.preventDefault();
-    setIndexOnEditMode('');
+    props.setIndexOnEditMode('');
+    const response = await fetch(
+      `https://jh-guest-list.herokuapp.com/${guest.id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          firstName: guest.firstName,
+          lastName: guest.lastName,
+        }),
+      },
+    );
+    await response.json();
+    props.mirrorState();
+  }
+  function cancelEdit(e) {
+    e.preventDefault();
+    props.setIndexOnEditMode('');
+    props.mirrorState();
   }
 
   function editFirstNameValueOnIndex(e, index) {
     e.preventDefault();
-    let stateBackup = props.guestList;
-    stateBackup[index] = [
-      e.currentTarget.value,
-      stateBackup[index][1],
-      stateBackup[index][2],
-      stateBackup[index][3],
-    ];
-    props.setGestList([...stateBackup]);
+    const guestListBackup = [...props.guestList];
+    guestListBackup[index].firstName = e.currentTarget.value;
+    props.setGuestList(guestListBackup);
   }
   function editLastNameValueOnIndex(e, index) {
     e.preventDefault();
-    let stateBackup = props.guestList;
-    stateBackup[index] = [
-      stateBackup[index][0],
-      e.currentTarget.value,
-      stateBackup[index][2],
-      stateBackup[index][3],
-    ];
-    props.setGestList([...stateBackup]);
+    const guestListBackup = [...props.guestList];
+    guestListBackup[index].lastName = e.currentTarget.value;
+    props.setGuestList(guestListBackup);
   }
 
-  function checkGuestCheckbox(index) {
-    const stateBackup = props.guestList;
-    stateBackup[index][2] = stateBackup[index][2] ? false : true;
-    props.setGestList([...stateBackup]);
+  async function checkGuestCheckbox(guest) {
+    const attendingState = !guest.attending;
+    const response = await fetch(
+      `https://jh-guest-list.herokuapp.com/${guest.id}`,
+      {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ attending: attendingState }),
+      },
+    );
+    await response.json();
+    props.mirrorState();
   }
 
-  function removeAll() {
-    props.setGestList([]);
+  async function removeAll() {
+    const promiseArray = props.guestList.map((guest) =>
+      fetch(`https://jh-guest-list.herokuapp.com/${guest.id}`, {
+        method: 'DELETE',
+      }),
+    );
+    await Promise.all(promiseArray);
+    localStorage.removeItem('dateIdRef');
+    props.setDateIdRef([]);
+    props.mirrorState();
   }
 
-  function filterAssist() {
-    setIsAssistFilterOn(true);
-    setIsNonAssistFilterOn(false);
+  function filterAttend() {
+    setIsAttendFilterOn(true);
+    setIsNonAttendFilterOn(false);
   }
 
-  function filterNonAssist() {
-    setIsNonAssistFilterOn(true);
-    setIsAssistFilterOn(false);
+  function filterNonAttend() {
+    setIsNonAttendFilterOn(true);
+    setIsAttendFilterOn(false);
   }
   function removeFilters() {
-    setIsNonAssistFilterOn(false);
-    setIsAssistFilterOn(false);
+    setIsNonAttendFilterOn(false);
+    setIsAttendFilterOn(false);
+  }
+
+  function datePrinter(dateRef) {
+    if (dateRef) {
+      const dateFromRef = new Date(dateRef);
+      const actualDate = Date.now();
+
+      return +dateFromRef - actualDate > 0
+        ? `RSVP deadline is ${dateFromRef.getDate()}/${
+            dateFromRef.getMonth() + 1
+          }/${dateFromRef.getFullYear()}`
+        : `Deadline overdue`;
+    }
+    return ``;
   }
   return (
     <>
       <button onClick={removeAll}>Remove all</button>
-      <button onClick={filterAssist}>Assistant</button>
-      <button onClick={filterNonAssist}>Non Assistant</button>
+      <button
+        onFocus={(e) => {
+          if (props.indexOnEditMode !== '') {
+            cancelEdit(e);
+          }
+        }}
+        onClick={filterAttend}
+      >
+        Attending
+      </button>
+      <button
+        onFocus={(e) => {
+          if (props.indexOnEditMode !== '') {
+            cancelEdit(e);
+          }
+        }}
+        onClick={filterNonAttend}
+      >
+        Non Attending
+      </button>
       <button onClick={removeFilters}>All</button>
-      {props.guestList.map((assistant, index) => (
+      {props.guestList.map((guest, index) => (
         <Form
-          isAssistent={assistant[2]}
-          isAssistFilterOn={isAssistFilterOn}
-          isNonAssistFilterOn={isNonAssistFilterOn}
-          key={assistant[3]}
+          isAttendent={guest.attending}
+          isAttendFilterOn={isAttendFilterOn}
+          isNonAttendFilterOn={isNonAttendFilterOn}
+          key={guest.id}
           onSubmit={(e) => e.preventDefault()}
         >
           <input
             type="checkbox"
-            checked={assistant[2]}
-            onChange={(e) => checkGuestCheckbox(index)}
+            checked={guest.attending}
+            onChange={() => checkGuestCheckbox(guest)}
+            onFocus={(e) => {
+              if (props.indexOnEditMode !== '') {
+                cancelEdit(e);
+              }
+            }}
           />
           <input
             type="text"
-            readOnly={indexOnEditMode !== index}
+            readOnly={props.indexOnEditMode !== index}
             onChange={(e) => editFirstNameValueOnIndex(e, index)}
-            value={assistant[0]}
+            value={guest.firstName}
           />
           <input
             type="text"
-            readOnly={indexOnEditMode !== index}
+            readOnly={props.indexOnEditMode !== index}
             onChange={(e) => editLastNameValueOnIndex(e, index)}
-            value={assistant[1]}
+            value={guest.lastName}
           />
+
           <EditButton
-            onScreen={indexOnEditMode === index ? true : false}
+            onScreen={props.indexOnEditMode === index ? true : false}
             onClick={(e) => editGuest(e, index)}
           >
             edit
           </EditButton>
           <EditButton
-            onScreen={indexOnEditMode !== index ? true : false}
+            onScreen={props.indexOnEditMode !== index ? true : false}
             type="submit"
-            onClick={(e) => doneEditGuest(e, index)}
+            onClick={(e) => doneEditGuest(e, guest)}
           >
-            done
+            save
           </EditButton>
-          <button onClick={(e) => removeGuest(e, index)}>X</button>
+          <EditButton
+            onScreen={props.indexOnEditMode !== index ? true : false}
+            type="submit"
+            onClick={(e) => cancelEdit(e)}
+          >
+            cancel
+          </EditButton>
+          <button onClick={(e) => removeGuest(e, guest)}>X</button>
+          <div>
+            {datePrinter(
+              props.dateIdRef.filter((ref) => ref.id === guest.id)[0]?.date,
+            )}
+          </div>
         </Form>
       ))}
     </>
